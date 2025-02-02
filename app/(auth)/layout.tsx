826a6db239f78be5774e024a77c0aac4a7b17235
afifaimrann <afifaimran844@gmail.com>
2025-02-02 18:37:43 +0600

    sign up and sing in page

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -4,8 +4,8 @@ import Image from 'next/image';
 const layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex min-h-screen">
-      <section className="bg-brand p-10">
-        <div>
+      <section className="bg-brand p-10 hidden w-1/2 items-center justify-center lg:flex xl:w-2/5">
+        <div className="flex max-h-[800px] max-w-[430px] flex-col justify-center space-y-12">
           <Image
             src="/assets/icons/logo-full.svg"
             alt="logo"
@@ -30,7 +30,18 @@ const layout = ({ children }: { children: React.ReactNode }) => {
         </div>
       </section>
 
-      {children}
+      <section className="flex flex-1 flex-col items-center bg-white p-4 py-10 lg:justify-center lg:p-10 lg:py-0">
+        <div className="mb-16 lg:hidden">
+          <Image
+            src="/assets/icons/logo-full-brand.svg"
+            alt="logo"
+            width={224}
+            height={82}
+            className="h-auto w-[200px] lg:w-[250px]"
+          />
+        </div>
+        {children}
+      </section>
     </div>
   );
 };
